e860854fa576b60b66aed0a6fe9c6959d1c1c334
lsh96900410 <lsh96900410@gmail.com>
2023-06-19 17:21:46 +0900

    15

--- 13.추상클래스[abstract]-제한자/src/EmployeePayPrintAbstractMain.java
+++ 13.추상클래스[abstract]-제한자/src/EmployeePayPrintAbstractMain.java
@@ -17,7 +17,7 @@ public class EmployeePayPrintAbstractMain {
 			System.out.println("------------------------------------------------------------");
 		
 		}
-		
+		System.out.println("커밋");
 		
 		
 	}
